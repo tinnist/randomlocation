5eb80e66b1834e85984c7117c3808e326a39539b
tinnist <82404492+tinnist@users.noreply.github.com>
2021-04-15 13:23:03 -0400

    Task 2 random

--- main.ts
+++ main.ts
@@ -1,4 +1,12 @@
-let mySprite = sprites.create(img`
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    sprite2.setPosition(randint(15, 145), randint(15, 105))
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    mySprite.setPosition(randint(15, 145), randint(15, 105))
+})
+let sprite2: Sprite = null
+let mySprite: Sprite = null
+mySprite = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -17,3 +25,21 @@ let mySprite = sprites.create(img`
     . . . . . . . . . . . . 3 . . . 
     `, SpriteKind.Player)
 mySprite.setPosition(randint(15, 145), randint(15, 105))
+sprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . 5 . . . . . . . . . 
+    . . . . . . . 5 . . . . . . . . 
+    . . . . . . . 5 . . . . . . . . 
+    . . . . . . . . 5 . . . . . . . 
+    . . . . . . . . 5 5 . . . . . . 
+    . . . . . . . . . 5 . . . . . . 
+    . . . . . . . . . . 5 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
